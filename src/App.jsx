e9fe76e9972e130e8e623e32a199d6523ee05125
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-07 02:39:24 -0800

    console error

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ const Statistics = ({ good, bad, neutral }) => {
       <p> No feedback given</p>
       : (
         <table>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good + neutral + bad} />
-          <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
-          <StatisticLine text="positive" value={`${(good) / (good + neutral + bad) * 100}%`} />
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good + neutral + bad} />
+            <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+            <StatisticLine text="positive" value={`${(good) / (good + neutral + bad) * 100}%`} />
+          </tbody>
         </table>)
     }
   </div>
