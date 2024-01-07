f697dfb1c2f27b8d8430bbfade9891d102886507
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-07 02:13:25 -0800

    1.9: unicafe step4

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react'
 
 const Statistics = ({ good, bad, neutral }) => {
+
   return <div>
     <h1>statistics</h1>
-    <p>good {`${good}`}</p>
-    <p>neutral {`${neutral}`}</p>
-    <p>bad {`${bad}`}</p>
-    <p>all {`${good + neutral + bad}`}</p>
-    <p>average {`${(good - bad) / (good + neutral + bad)}`}</p>
-    <p>positive {`${(good) / (good + neutral + bad) * 100}`}%</p></div>
+    {(good === 0 && bad === 0 && neutral === 0) ?
+      <p> No feedback given</p>
+      : (
+        <div>
+          <p>good {`${good}`}</p>
+          <p>neutral {`${neutral}`}</p>
+          <p>bad {`${bad}`}</p>
+          <p>all {`${good + neutral + bad}`}</p>
+          <p>average {`${(good - bad) / (good + neutral + bad)}`}</p>
+          <p>positive {`${(good) / (good + neutral + bad) * 100}`}%</p>
+        </div>)
+    }
+  </div>
 }
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
