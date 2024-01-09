5df0c97d8e8e38a8306989ab0832b47026117f27
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-09 01:08:18 -0800

    use axios, json-server for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [nameSearch, setNameSearch] = useState('')
-
+  useEffect(() => {
+    async function fetch() {
+      const res = await axios.get("http://localhost:3001/persons")
+      console.log("🚀 ~ fetch ~ res:", res)
+      setPersons(res.data)
+    }
+    fetch()
+  }, [])
   const handleSubmit = (name, number) => {
     if (persons.some(person => person.name === name)) {
       alert(`${name} is already added to phonebook`)
