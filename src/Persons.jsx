ca6709e7a5a99d651776cd7428c4abdade7c3dac
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-09 22:24:04 -0800

    clean up

--- src/Persons.jsx
+++ src/Persons.jsx
@@ -1,7 +1,7 @@
 function Persons({ persons, nameSearch, handleDelete }) {
     return <ul>
         {persons.filter(person => person.name.toLowerCase().includes(nameSearch)).map(person =>
-            <li>{person.name} {person.number} <button onClick={() => handleDelete(person)}>Delete</button>
+            <li key={person.id}>{person.name} {person.number} <button onClick={() => handleDelete(person)}>Delete</button>
             </li>)}
     </ul>
 }
