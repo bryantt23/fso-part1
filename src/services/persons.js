684c58246cb322f068323ecf3bb6ce71ae5f0cb7
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-11 04:10:37 -0800

    use api

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://render-test-x6o8.onrender.com/api/persons';
 
 export const getAll = () => {
   return axios.get(baseUrl);
